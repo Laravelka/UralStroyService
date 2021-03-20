b40e3340c7af6df01d9c9257d53deef64a2a4230
Laravelka <joker777237@gmail.com>
2021-03-20 07:19:50 +0300

    Changed the project name and added push notifications

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,67 @@
 </template>
 
 <script lang="ts">
-	import { IonApp, IonRouterOutlet } from '@ionic/vue';
+	import axios from 'axios';
 	import { defineComponent } from 'vue';
+	import { IonApp, IonRouterOutlet } from '@ionic/vue';
+	import { 
+		Plugins,
+		PushNotification,
+		PushNotificationToken,
+		PushNotificationActionPerformed
+	} from '@capacitor/core';
 
 	export default defineComponent({
 		name: 'App',
 		components: {
 			IonApp,
 			IonRouterOutlet
+		},
+		setup() {
+			const { PushNotifications } = Plugins;
+			const pushToken = localStorage.getItem('pushToken') ?? false;
+
+			PushNotifications.requestPermission().then((result: any) => {
+				if (result.granted) {
+					PushNotifications.register();
+				} else {
+					console.error('Права для push уведомлений не выданы')
+				}
+			});
+
+			if (!pushToken) {
+				PushNotifications.addListener('registration',
+					(token: PushNotificationToken) => {
+						axios.post('user/subscribe', {token: token.value}).then((response: any) => {
+							console.log(JSON.stringify(response));
+
+							localStorage.setItem('pushToken', token.value);
+						}).catch((error: any) => {
+							console.error(JSON.stringify(error.response));
+						});
+					}
+				);
+			}
+
+			PushNotifications.addListener('registrationError',
+				(error: any) => {
+					console.error('Error on registration: ' + JSON.stringify(error));
+				}
+			);
+
+			// Show us the notification payload if the app is open on our device
+			PushNotifications.addListener('pushNotificationReceived',
+				(notification: PushNotification) => {
+					console.log('notifyRecevied:', JSON.stringify(notification))
+				}
+			);
+
+			// Method called when tapping on a notification
+			PushNotifications.addListener('pushNotificationActionPerformed',
+				(notification: PushNotificationActionPerformed) => {
+					console.log('notifyPerformed:', JSON.stringify(notification))
+				}
+			);
 		}
 	});
 </script>
